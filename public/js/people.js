cf3703c82136cfe0251cf5b69f635bae5fffad1e
travisdjones78 <travisdjones2000@gmail.com>
2022-12-15 23:02:59 -0500

    Update people.js

--- public/js/people.js
+++ public/js/people.js
@@ -3,14 +3,14 @@ $.get('/people/get-all-people', data => {
 })
 
 function renderPeople(people) {
-    people.forEach((person, idx) => {
+    people.forEach((person) => {
         $('#people').append(`
             <div class="card col-2 ml-2 mr-2 mt-2 " >
             <img src="./img/characters/${person.name}.jpg" class="card-img-top" alt="No image loaded">
             <div class="card-body">
               <h5 class="card-title">${person.name}</h5>
               <p class="card-text"></p>
-              <a href="./person.html?${idx}" class="btn btn-sm btn-warning">Read More About ${person.name}</a>
+              <a href="./person.html?${person.uid}" class="btn btn-sm btn-warning">Read More About ${person.name}</a>
             </div>
           </div>
             `)
