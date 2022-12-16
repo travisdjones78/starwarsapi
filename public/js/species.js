b5f8d24536119f2fc661e752fa7c04e14a9f593d
travisdjones78 <travisdjones2000@gmail.com>
2022-12-15 23:08:50 -0500

    Update species.js

--- public/js/species.js
+++ public/js/species.js
@@ -4,14 +4,14 @@ $.get('/species/get-all-species', data => {
 
 function renderSpecies(species) {
     console.log(species)
-    species.forEach((species, idx) => {
+    species.forEach((species) => {
         $('#species').append(`
         <div class="card col-2 ml-2 mr-2 mt-2 " >
         <img src="./img/species/${species.name}.jpg" class="card-img-top" alt="No image loaded">
         <div class="card-body">
           <h5 class="card-title">${species.name}</h5>
           <p class="card-text"></p>
-          <a href="./oneSpecies.html?${idx}" class="btn btn-sm btn-warning">Read More About The ${species.name} Species</a>
+          <a href="./oneSpecies.html?${species.uid}" class="btn btn-sm btn-warning">Read More About The ${species.name} Species</a>
         </div>
       </div>
         `)
